3548863e9336fec60d4aea3ce172288649e398e1
dyronrh <dyronrh@yahoo.es>
2025-04-08 19:33:55 -0400

    fix dependicye

--- secrets.sh
+++ secrets.sh
@@ -3,5 +3,5 @@ kubectl create secret generic n8n-secrets \
   --from-literal=POSTGRES_PASSWORD=supersecurepass \
   --from-literal=N8N_BASIC_AUTH_USER=admin \
   --from-literal=N8N_BASIC_AUTH_PASSWORD=strongpassword \
-  --from-literal=N8N_BASIC_HOST=https://fe81-186-107-66-123.ngrok-free.app \
+  --from-literal=N8N_BASIC_HOST=https://hostname \
   --from-literal=N8N_ENCRYPTION_KEY=$(openssl rand -hex 32)
