024a1a6f0f1161a413d28020b1a3fa9175ef1729
dyronrh <dyronrh@yahoo.es>
2025-04-08 14:31:55 -0400

    add changes

--- secrets.sh
+++ secrets.sh
@@ -3,5 +3,4 @@ kubectl create secret generic n8n-secrets \
   --from-literal=POSTGRES_PASSWORD=supersecurepass \
   --from-literal=N8N_BASIC_AUTH_USER=admin \
   --from-literal=N8N_BASIC_AUTH_PASSWORD=strongpassword \
-  --from-literal=N8N_LB_IP=192.168.1.151 \
   --from-literal=N8N_ENCRYPTION_KEY=$(openssl rand -hex 32)
